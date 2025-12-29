0f4929dcd0e5c5ee8a1f11dcfcce305c70a28a32
taslimanisan <152017842+taslimanisan@users.noreply.github.com>
2025-12-29 11:14:14 +0530

    Add background image to dashboard content

--- src/component/Dashboard/Dashboard.jsx
+++ src/component/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
 import styles from './Dashboard.module.css';
 
-
 const Dashboard = () => {
-
   return (
     <div className={styles['dashboard-container']}>
-      <h1 className={styles['dashboard-title']}>Welcome </h1>
-      {/* Dashboard content */}
-      <div className="empty-dashboard">
+      <h1 className={styles['dashboard-title']}>Welcome</h1>
+
+      {/* Dashboard content with background */}
+      <div className={styles['dashboard-content']}>
+        {/* Your dashboard widgets / cards will go here */}
       </div>
     </div>
   );
