2fcd7fdc0373c96630bb1dc25e3179ace1675899
taslimanisan <152017842+taslimanisan@users.noreply.github.com>
2025-12-30 13:06:31 +0530

    Add backend server

---
+++ server.js
@@ -0,0 +1,22 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+
+server.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const user = router.db.get('users').find({ email, password }).value();
+  if (user) {
+    res.json({ token: 'fake-token' });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
+server.use(router);
+server.listen(5000, () => {
+  console.log('JSON Server is running on port 5000');
+});
